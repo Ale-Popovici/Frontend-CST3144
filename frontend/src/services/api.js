081cff61ca7a6abcd02d024ff01cc2291351d4d4
Alex Popovici <138364277+Ale-Popovici@users.noreply.github.com>
2024-11-17 16:17:31 +0000

    change point

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@
 const API_URL =
   process.env.VUE_APP_API_URL ||
   (window.location.hostname === "ale-popovici.github.io"
-    ? "https://ec2-3-253-62-183.eu-west-1.compute.amazonaws.com/api/lessons"
+    ? "https://ec2-3-253-62-183.eu-west-1.compute.amazonaws.com/"
     : "http://localhost:5001/api");
 
 console.log("API URL:", API_URL); //add
